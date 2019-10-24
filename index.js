10682dd8a8396a722168d8d5cd788065d6d9017d
Ahmed Abououf <aabuauf@yahoo.com>
2019-10-24 19:03:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution he
 function findMatching(drivers,string){
   return drivers.filter(d=> {
-    return d.uppercase==string.uppercase
+    return d.touppercase()===string.touppercase()
   })
 }
